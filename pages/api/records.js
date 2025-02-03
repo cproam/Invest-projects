4a841aaf568c87276b001defbeca9093c796d2df
Almas <gdocs34@gmail.com>
2025-02-03 16:52:38 +0300

    upgrade

--- pages/api/records.js
+++ pages/api/records.js
@@ -7,12 +7,14 @@ export default async function handler(req, res) {
     optionsSuccessStatus: 200,
   });
 
+  // console.log(process.env.DB_HOST);
+
   let response = await fetch(
     process.env.DB_HOST +
-      "?" +
-      new URLSearchParams({
-        records: "",
-      })
+    "?" +
+    new URLSearchParams({
+      records: "",
+    })
   );
   let json = await response.json();
   res.status(200).json({ message: "Форма отправлена", records: json });
